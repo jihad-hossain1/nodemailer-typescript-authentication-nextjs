75f981386492c6efd2031871139bc280e9cb89db
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-18 23:35:25 +0600

    user register api done

--- src/app/api/users/register/route.ts
+++ src/app/api/users/register/route.ts
@@ -1,13 +1,58 @@
 import { connectDatabase } from "@/config/databaseConnection";
+import User from "@/models/user.model";
+import { validateJSON } from "@/utils/validJSON";
 import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
 
 connectDatabase();
 
 export async function POST(request: NextRequest) {
+  const requestFromBody = await request.json();
   try {
-    const requestFromBody = await request.json();
+    // check valid json or not
+    const validjson = validateJSON(requestFromBody);
 
-    const { fullname, email, password, username } = requestFromBody;
+    if (validjson === true) {
+      const { fullname, email, password, username } = requestFromBody;
+
+      // check provided field given are not fullname && email && password && username
+      if (!(fullname && email && password && username)) {
+        return NextResponse.json(
+          { message: "fullname || email || password || username are required" },
+          { status: 400 }
+        );
+      }
+      // check user are alredy exist or not
+      const userExist = await User.findOne({ email: email });
+
+      if (userExist) {
+        return NextResponse.json(
+          { message: "Email already exist" },
+          { status: 400 }
+        );
+      }
+      // passowrd hashed generate
+      const salt = await bcryptjs.genSalt(10);
+      const hashedPassword = await bcryptjs.hash(password, salt);
+
+      // save new user on database
+      const newUser = new User({
+        fullname,
+        email,
+        password: hashedPassword,
+        username,
+      });
+
+      const savedUser = await newUser.save();
+
+      return NextResponse.json({
+        message: "user created successfully",
+        success: true,
+        savedUser,
+      });
+    } else {
+      return NextResponse.json({ message: "inavlid fields: ", validjson });
+    }
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
