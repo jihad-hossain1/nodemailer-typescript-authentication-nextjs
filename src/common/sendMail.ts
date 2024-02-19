c5d62908faa3da81672a61df660af149960f8a0a
jihad-hossain1 <jihadkhan934@gmail.com>
2024-02-19 11:27:47 +0600

    some change for when email are send then deploy problem

--- src/common/sendMail.ts
+++ src/common/sendMail.ts
@@ -19,8 +19,8 @@ export const sendEmail = async ({ email, emailType, userId }: any) => {
     }
 
     var mailTransport = nodemailer.createTransport({
-      host: process.env.MAIL_HOST!,
-      port: process.env.MAIL_PORT!,
+      host: process.env.MAIL_HOST! as string,
+      port: Number(process.env.MAIL_PORT!),
       auth: {
         user: process.env.MAIL_USER!,
         pass: process.env.MAIL_PASS!,
